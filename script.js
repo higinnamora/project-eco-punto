f12d7af62bb07405b648e10d07560fed1f158c60
higinnamora <higinnamora@gmail.com>
2025-10-17 22:46:28 -0500

    EcoPunto: Navigation settings, hamburger menu, image optimization

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ function initMap() {
     title: 'Bogotá',
   });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+});
